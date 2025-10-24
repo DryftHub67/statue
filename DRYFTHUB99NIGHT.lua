f5b6e362fc7d2e8a692391b808ca98a1ae83256c
Dryft-Hub <symphonyhub67@gmail.com>
2025-10-24 21:05:09 +0800

    Create DRYFTHUB99NIGHT.lua

---
+++ DRYFTHUB99NIGHT.lua
@@ -0,0 +1,28 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+if not game:IsLoaded() then repeat task.wait() until game:IsLoaded() end
+
+if getgenv().RunScript == true then return end
+getgenv().RunScript = true
+
+getgenv().WebhookURL = "https://discord.com/api/webhooks/1427252441644273694/z_KaDRhu7RCQl0letv40k4LA8mI79a8pOxVDDhSonHyZMubXLZ5SXXoinDTn5wxWhsl7"
+getgenv().AutoFarm = true
+getgenv().hop = "old"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DryftHub67/statue/refs/heads/main/F99D.lua"))()
+
+local queueScript = [[
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/kuy/refs/heads/main/dyhub99night.lua"))()
+]]
+queue_on_teleport(queueScript)
+
+repeat wait(0.1) until game.Players
+local TimeWaitLoadGame = 0
+local player = game.Players.LocalPlayer
+repeat
+	wait(0.1)
+	TimeWaitLoadGame = TimeWaitLoadGame + 0.1
+	if TimeWaitLoadGame > 3 then
+		player:Kick()
+		wait(0.5)
+		game:GetService("TeleportService"):Teleport(126509999114328)
+	end
+until game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
